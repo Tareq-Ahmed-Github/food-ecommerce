4522e718f7b2a57972f661c270288468b0fe7bb2
unknown <tareqrobin57@gmail.com>
2022-06-08 14:50:07 +0600

    react, react-router, tailwind, daisyui setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Header from './Components/Common/Header';
+import Footer from './Components/Common/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+      <Footer/>     
+    </div>
+  );
+}
+
+export default App;
